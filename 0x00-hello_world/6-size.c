6e69eef18191f48e6de9c08423f780d1e20d1924
zobalaura <laurazobs6@gmail.com>
2022-09-08 18:27:50 -0700

    let's code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %dbytes\n", sizeof(char));
+	printf("Size of an int: %dbytes\n", sizeof(int));
+	printf("Size of a double: %dbytes\n", sizeof(double));
+	printf("Size of a float: %dbytes\n", sizeof(float));
+	return (0);
+}
