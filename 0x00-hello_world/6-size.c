adafb37ade641a128cf8c640de9359f3facdf51d
zobalaura <laurazobs6@gmail.com>
2022-09-08 18:35:28 -0700

    let's code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,8 @@ int main(void)
 {
 	printf("Size of a char: %dbytes\n", sizeof(char));
 	printf("Size of an int: %dbytes\n", sizeof(int));
-	printf("Size of a double: %dbytes\n", sizeof(double));
+	printf("Size of a long int: %dbytes\n", sizeof(long int));
+	printf("Size of a long long int: %dbytes\n", sizeof(long long int));
 	printf("Size of a float: %dbytes\n", sizeof(float));
 	return (0);
 }
