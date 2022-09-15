dbbc2f6e68994c6a64508b832876207cfd4060dc
zobalaura <laurazobs6@gmail.com>
2022-09-15 11:50:54 -0700

    -

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit -  a function that checks for a digit (0 through 9).
+ * Return: 1 if c is a digi and 0 if otherwise.
+ * @c : type int number
+ */
+int _isdigit(int c)
+{
+	if (c >= 0  || c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul -  a function that multiplies two integers.
+ * @a : type int number
+ * @b : type int number
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	_putchar('x\n');
+	return (0);
+}
