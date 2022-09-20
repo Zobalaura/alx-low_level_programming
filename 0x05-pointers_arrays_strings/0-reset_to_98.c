b5c295138aafca0dcbb6d5c703f5d60ce93e2303
zobalaura <laurazobs6@gmail.com>
2022-09-19 17:35:49 -0700

    -

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - a function
+ * Description -  a function that updates parameter
+ * @n: a pointer of int type
+ * Return : void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
